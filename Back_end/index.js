b6203d9f00d729de80783abaa7f1133e1e70559b
‘SuaNguyen2004’ <abc@gmail.com>
2026-09-24 01:30:20 +0700

    Test Deploy Part1

--- Back_end/index.js
+++ Back_end/index.js
@@ -169,7 +169,8 @@ app.use(
                 !origin ||
                 allowedOrigins.includes(origin) ||
                 origin.startsWith("http://localhost:") ||
-                origin.startsWith("http://127.0.0.1:")
+                origin.startsWith("http://127.0.0.1:") ||
+                origin.endsWith(".trycloudflare.com")
             ) {
                 return callback(null, true);
             }
